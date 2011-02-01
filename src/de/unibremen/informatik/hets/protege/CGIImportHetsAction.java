c235a726bbcb2f38ed3d06b6fbc74b874911b388
josch <josch@pyneo.org>
2011-02-01 12:27:46 +0100

    add stubs for new import options

---
+++ src/de/unibremen/informatik/hets/protege/CGIImportHetsAction.java
@@ -0,0 +1,22 @@
+package de.unibremen.informatik.hets.protege;
+
+import java.awt.event.ActionEvent;
+
+import org.protege.editor.owl.ui.action.ProtegeOWLAction;
+
+public class CGIImportHetsAction extends ProtegeOWLAction {
+
+	private static final long serialVersionUID = -4056096587762591108L;
+
+	@Override
+	public void initialise() throws Exception {
+	}
+
+	@Override
+	public void dispose() throws Exception {
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent event) {
+	}
+}
